f278bfcd23824ed37d61bb606a29597dfad8be61
Dmitry Turchenko <dmitrytyt@ya.ru>
2026-04-17 16:47:58 +0300

    Добавлено верефицирование для стилей

--- app/Core/View.php
+++ app/Core/View.php
@@ -6,6 +6,8 @@ use Smarty\Smarty;
 
 class View
 {
+    private const CSS_PATH = '/assets/css/style.css';
+
     public static function make(): self
     {
         return new self();
@@ -19,6 +21,7 @@ class View
         $this->smarty->setCacheDir(Config::get('app.cache_dir'));
         $this->smarty->assign('appName', Config::get('app.name'));
         $this->smarty->assign('baseUrl', Config::get('app.base_url'));
+        $this->smarty->assign('styleVersion', $this->resolveAssetVersion(self::CSS_PATH));
     }
 
     public function render(string $template, array $data = []): void
@@ -28,5 +31,16 @@ class View
         }
 
         $this->smarty->display($template . '.tpl');
+    }
+
+    private function resolveAssetVersion(string $assetPath): string
+    {
+        $publicPath = dirname(__DIR__, 2) . '/public' . $assetPath;
+
+        if (!is_file($publicPath)) {
+            return 'dev';
+        }
+
+        return (string) filemtime($publicPath);
     }
 }
--- templates/layouts/main.tpl
+++ templates/layouts/main.tpl
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{$pageTitle|default:$appName} - {$appName}</title>
-    <link rel="stylesheet" href="{$baseUrl}/assets/css/style.css">
+    <link rel="stylesheet" href="{$baseUrl}/assets/css/style.css?v={$styleVersion}">
 </head>
 <body>
 <header class="site-header">
